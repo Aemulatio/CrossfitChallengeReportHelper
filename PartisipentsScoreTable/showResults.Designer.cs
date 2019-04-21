6eb0ff9a9c9fc735455e88df0358bf584f254119
Stepan S <dumn2011@gmail.com>
2019-04-21 18:04:56 +0900

    Unbound and deleted all DB connectings

--- PartisipentsScoreTable/showResults.Designer.cs
+++ PartisipentsScoreTable/showResults.Designer.cs
@@ -28,45 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.challengerDataGridView = new System.Windows.Forms.DataGridView();
-            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.challengerBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.challengerDBDataSet = new PartisipentsScoreTable.ChallengerDBDataSet();
-            this.challengerTableAdapter = new PartisipentsScoreTable.ChallengerDBDataSetTableAdapters.ChallengerTableAdapter();
-            this.tableAdapterManager = new PartisipentsScoreTable.ChallengerDBDataSetTableAdapters.TableAdapterManager();
             this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.challengerDataGridView)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.challengerBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.challengerDBDataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // challengerDataGridView
             // 
             this.challengerDataGridView.AllowUserToAddRows = false;
             this.challengerDataGridView.AllowUserToDeleteRows = false;
-            this.challengerDataGridView.AutoGenerateColumns = false;
             this.challengerDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.challengerDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.challengerDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.dataGridViewTextBoxColumn1,
-            this.dataGridViewTextBoxColumn2,
-            this.dataGridViewTextBoxColumn3,
-            this.dataGridViewTextBoxColumn4,
-            this.dataGridViewTextBoxColumn5,
-            this.dataGridViewTextBoxColumn6,
-            this.dataGridViewTextBoxColumn7,
-            this.dataGridViewTextBoxColumn8,
-            this.dataGridViewTextBoxColumn9});
-            this.challengerDataGridView.DataSource = this.challengerBindingSource;
             this.challengerDataGridView.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.challengerDataGridView.Location = new System.Drawing.Point(0, 30);
             this.challengerDataGridView.Name = "challengerDataGridView";
@@ -74,89 +46,6 @@
             this.challengerDataGridView.Size = new System.Drawing.Size(945, 549);
             this.challengerDataGridView.TabIndex = 1;
             this.challengerDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.challengerDataGridView_CellContentClick);
-            // 
-            // dataGridViewTextBoxColumn1
-            // 
-            this.dataGridViewTextBoxColumn1.DataPropertyName = "Number";
-            this.dataGridViewTextBoxColumn1.HeaderText = "№";
-            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
-            this.dataGridViewTextBoxColumn1.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn2
-            // 
-            this.dataGridViewTextBoxColumn2.DataPropertyName = "Name";
-            this.dataGridViewTextBoxColumn2.HeaderText = "Name";
-            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
-            this.dataGridViewTextBoxColumn2.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn3
-            // 
-            this.dataGridViewTextBoxColumn3.DataPropertyName = "25";
-            this.dataGridViewTextBoxColumn3.HeaderText = "25";
-            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
-            this.dataGridViewTextBoxColumn3.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn4
-            // 
-            this.dataGridViewTextBoxColumn4.DataPropertyName = "35";
-            this.dataGridViewTextBoxColumn4.HeaderText = "35";
-            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
-            this.dataGridViewTextBoxColumn4.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn5
-            // 
-            this.dataGridViewTextBoxColumn5.DataPropertyName = "45";
-            this.dataGridViewTextBoxColumn5.HeaderText = "45";
-            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
-            this.dataGridViewTextBoxColumn5.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn6
-            // 
-            this.dataGridViewTextBoxColumn6.DataPropertyName = "60";
-            this.dataGridViewTextBoxColumn6.HeaderText = "60";
-            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
-            this.dataGridViewTextBoxColumn6.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn7
-            // 
-            this.dataGridViewTextBoxColumn7.DataPropertyName = "70";
-            this.dataGridViewTextBoxColumn7.HeaderText = "70";
-            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
-            this.dataGridViewTextBoxColumn7.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn8
-            // 
-            this.dataGridViewTextBoxColumn8.DataPropertyName = "80";
-            this.dataGridViewTextBoxColumn8.HeaderText = "80";
-            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
-            this.dataGridViewTextBoxColumn8.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn9
-            // 
-            this.dataGridViewTextBoxColumn9.DataPropertyName = "100";
-            this.dataGridViewTextBoxColumn9.HeaderText = "100";
-            this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
-            this.dataGridViewTextBoxColumn9.ReadOnly = true;
-            // 
-            // challengerBindingSource
-            // 
-            this.challengerBindingSource.DataMember = "Challenger";
-            this.challengerBindingSource.DataSource = this.challengerDBDataSet;
-            // 
-            // challengerDBDataSet
-            // 
-            this.challengerDBDataSet.DataSetName = "ChallengerDBDataSet";
-            this.challengerDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // challengerTableAdapter
-            // 
-            this.challengerTableAdapter.ClearBeforeFill = true;
-            // 
-            // tableAdapterManager
-            // 
-            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
-            this.tableAdapterManager.ChallengerTableAdapter = this.challengerTableAdapter;
-            this.tableAdapterManager.UpdateOrder = PartisipentsScoreTable.ChallengerDBDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
             // 
             // button1
             // 
@@ -179,28 +68,12 @@
             this.Size = new System.Drawing.Size(945, 579);
             this.Load += new System.EventHandler(this.showResults_Load);
             ((System.ComponentModel.ISupportInitialize)(this.challengerDataGridView)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.challengerBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.challengerDBDataSet)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private ChallengerDBDataSet challengerDBDataSet;
-        private System.Windows.Forms.BindingSource challengerBindingSource;
-        private ChallengerDBDataSetTableAdapters.ChallengerTableAdapter challengerTableAdapter;
-        private ChallengerDBDataSetTableAdapters.TableAdapterManager tableAdapterManager;
         private System.Windows.Forms.DataGridView challengerDataGridView;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
         private System.Windows.Forms.Button button1;
     }
 }
--- PartisipentsScoreTable/showResults.cs
+++ PartisipentsScoreTable/showResults.cs
@@ -22,25 +22,9 @@ namespace PartisipentsScoreTable
 
         private void showResults_Load(object sender, EventArgs e)
         {
-            //TODO: Load data from DB to dataGridView (it should be it 2nd course's courseWork)
 
-            try
-            {
-                // TODO: данная строка кода позволяет загрузить данные в таблицу "course_workDataSet.Zakaz". При необходимости она может быть перемещена или удалена.
-                this.challengerTableAdapter.Fill(this.challengerDBDataSet.Challenger);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
         }
-
-        private void challengerBindingNavigatorSaveItem_Click(object sender, EventArgs e)
-        {
-            this.Validate();
-            this.challengerBindingSource.EndEdit();
-            this.tableAdapterManager.UpdateAll(this.challengerDBDataSet);
-        }
+        
 
         private void button1_Click(object sender, EventArgs e)
         {
