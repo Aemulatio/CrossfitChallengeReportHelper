a6de9393b01ad23f45d760ae5d632745c6cc4386
Stepan S <dumn2011@gmail.com>
2019-04-22 01:08:44 +0900

    Done

--- PartisipentsScoreTable/showResults.Designer.cs
+++ PartisipentsScoreTable/showResults.Designer.cs
@@ -30,6 +30,16 @@
         {
             this.challengerDataGridView = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.w25 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.w35 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.w45 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.w60 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.w70 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.w80 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.w100 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sumOfAll = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.challengerDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,6 +49,17 @@
             this.challengerDataGridView.AllowUserToDeleteRows = false;
             this.challengerDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.challengerDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.challengerDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.number,
+            this.chName,
+            this.w25,
+            this.w35,
+            this.w45,
+            this.w60,
+            this.w70,
+            this.w80,
+            this.w100,
+            this.sumOfAll});
             this.challengerDataGridView.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.challengerDataGridView.Location = new System.Drawing.Point(0, 30);
             this.challengerDataGridView.Name = "challengerDataGridView";
@@ -50,13 +71,83 @@
             // button1
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Location = new System.Drawing.Point(0, 0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(945, 30);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Обновить";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // number
+            // 
+            this.number.FillWeight = 50F;
+            this.number.HeaderText = "#";
+            this.number.Name = "number";
+            this.number.ReadOnly = true;
+            // 
+            // chName
+            // 
+            this.chName.FillWeight = 150F;
+            this.chName.HeaderText = "Name";
+            this.chName.Name = "chName";
+            this.chName.ReadOnly = true;
+            // 
+            // w25
+            // 
+            this.w25.FillWeight = 83.9467F;
+            this.w25.HeaderText = "25";
+            this.w25.Name = "w25";
+            this.w25.ReadOnly = true;
+            // 
+            // w35
+            // 
+            this.w35.FillWeight = 83.9467F;
+            this.w35.HeaderText = "35";
+            this.w35.Name = "w35";
+            this.w35.ReadOnly = true;
+            // 
+            // w45
+            // 
+            this.w45.FillWeight = 83.9467F;
+            this.w45.HeaderText = "45";
+            this.w45.Name = "w45";
+            this.w45.ReadOnly = true;
+            // 
+            // w60
+            // 
+            this.w60.FillWeight = 83.9467F;
+            this.w60.HeaderText = "60";
+            this.w60.Name = "w60";
+            this.w60.ReadOnly = true;
+            // 
+            // w70
+            // 
+            this.w70.FillWeight = 83.9467F;
+            this.w70.HeaderText = "70";
+            this.w70.Name = "w70";
+            this.w70.ReadOnly = true;
+            // 
+            // w80
+            // 
+            this.w80.FillWeight = 83.9467F;
+            this.w80.HeaderText = "80";
+            this.w80.Name = "w80";
+            this.w80.ReadOnly = true;
+            // 
+            // w100
+            // 
+            this.w100.FillWeight = 83.9467F;
+            this.w100.HeaderText = "100";
+            this.w100.Name = "w100";
+            this.w100.ReadOnly = true;
+            // 
+            // sumOfAll
+            // 
+            this.sumOfAll.HeaderText = "Sum";
+            this.sumOfAll.Name = "sumOfAll";
+            this.sumOfAll.ReadOnly = true;
             // 
             // showResults
             // 
@@ -75,5 +166,15 @@
         #endregion
         private System.Windows.Forms.DataGridView challengerDataGridView;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn chName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn w25;
+        private System.Windows.Forms.DataGridViewTextBoxColumn w35;
+        private System.Windows.Forms.DataGridViewTextBoxColumn w45;
+        private System.Windows.Forms.DataGridViewTextBoxColumn w60;
+        private System.Windows.Forms.DataGridViewTextBoxColumn w70;
+        private System.Windows.Forms.DataGridViewTextBoxColumn w80;
+        private System.Windows.Forms.DataGridViewTextBoxColumn w100;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sumOfAll;
     }
 }
--- PartisipentsScoreTable/showResults.cs
+++ PartisipentsScoreTable/showResults.cs
@@ -4,10 +4,12 @@ using System.ComponentModel;
 using System.Drawing;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 //TODO: May be do some kind of report
 
@@ -22,19 +24,59 @@ namespace PartisipentsScoreTable
 
         private void showResults_Load(object sender, EventArgs e)
         {
+            try
+            {
+                if (!File.Exists(@".\Challengers.xml"))
+                {
+                    using (StreamWriter sr = File.CreateText(@".\Challengers.xml"))
+                    {
+                        sr.WriteLine("<?xml version=\"1.0\" encoding=\"utf - 8\" ?>");
+                        sr.WriteLine("< challengers >");
+                        sr.WriteLine("</ challengers >");
+                    }
+                }
 
+                XDocument xDoc = XDocument.Load("Challengers.xml");
+                var result = from VAR in xDoc.Root.Descendants("Challenger")
+                    select VAR;
+
+                foreach (var VARIABLE in result)
+                {
+                    challengerDataGridView.Rows.Add(
+                        VARIABLE.Attribute("Number").Value
+                        , VARIABLE.Attribute("Name").Value
+                        , VARIABLE.Attribute("w25").Value
+                        , VARIABLE.Attribute("w35").Value
+                        , VARIABLE.Attribute("w45").Value
+                        , VARIABLE.Attribute("w60").Value
+                        , VARIABLE.Attribute("w70").Value
+                        , VARIABLE.Attribute("w80").Value
+                        , VARIABLE.Attribute("w100").Value
+                        , (int.Parse(VARIABLE.Attribute("w25").Value) * 25 +
+                           int.Parse(VARIABLE.Attribute("w35").Value) * 35 +
+                           int.Parse(VARIABLE.Attribute("w45").Value) * 45 +
+                           int.Parse(VARIABLE.Attribute("w60").Value) * 60 +
+                           int.Parse(VARIABLE.Attribute("w70").Value) * 70 +
+                           int.Parse(VARIABLE.Attribute("w80").Value) * 80 +
+                           int.Parse(VARIABLE.Attribute("w100").Value) * 100)
+                    );
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
         }
-        
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            challengerDataGridView.Refresh();
-            challengerDataGridView.Update();
+            challengerDataGridView.Rows.Clear();
+            showResults_Load(sender,e);
         }
 
         private void challengerDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
         }
     }
 }
